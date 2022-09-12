52272f9575a3bb78510142f2e0ab55b1db7133c2
Federico Peluffo <fedepel@gmail.com>
2022-09-12 20:34:45 -0300

    preentrega1

--- js/main.js
+++ js/main.js
@@ -9,41 +9,41 @@ function agregarCompras() {
     let numeroCompras = parseInt(
         prompt("Cuantos compras necesita registrar en su carrito?")
     );
+
     let compras = [];
+
     for (let index = 0; index < numeroCompras; index++) {
         let nombre = prompt("Ingresa el nombre del articulo comprado.");
         let costo = parseFloat(prompt("Ingresa el costo del producto."));
         let objetoCompra = new Compra(nombre, costo)
         compras.push(objetoCompra);
     }
+
     return compras;
 }
 
 function calcularCosto(compras) {
-    let sumatoriaCosto = 0;
-    for (const compra of compras) {
-        sumatoriaCosto += compra.costo;
-    }
+    let sumatoriaCosto = compras.reduce((acc, el) => acc + el.costo, 0)
     return sumatoriaCosto;
 }
 
 function valorCuota(costoTotal, pagoAdelanto, numeroCuotas) {
     let total = 0;
-    
+
     if (pagoAdelanto > 0) {
         total = costoTotal - pagoAdelanto;
     } else {
         total = costoTotal
     }
-    
+
     let cuotaMes = 0
-    
+
     if (numeroCuotas <= 6) {
         cuotaMes = total / numeroCuotas;
     } else {
         cuotaMes = total * 1.05 / numeroCuotas;
     }
-    
+
     return cuotaMes
 }
 
@@ -56,11 +56,54 @@ function avisoFinal(costoTotal, numeroCuotas, cuotaMes, pagoAdelanto) {
         alert('Ningún dato ha sido ingresado.')
     }
 }
+function mostrarMenu() {
+    const OPCION = prompt(
+        'Bienvenido, seleccione una opción (ESC para salir)\n1. Agregar compras\n2. Eliminar compras\n3. Definir forma de pago');
+    return OPCION;
+}
+
+function procesarInventario() {
+    let opcionSeleccionada = mostrarMenu();
+    while (opcionSeleccionada?.toLowerCase() != "esc") {
+        if (opcionSeleccionada != "") {
+            opcionSeleccionada = parseInt(opcionSeleccionada);
+            if (!isNaN(opcionSeleccionada)) {
+                switch (opcionSeleccionada) {
+                    case 1:
+                        miProducto = obtenerDatosDeProducto();
+                        break;
+                    case 2:
+                        const PRECIO_A_AUMENTAR = parseFloat(
+                            prompt("Ingrese en cuanto aumenta el precio del producto")
+                        );
+                        miProducto.aumentarPrecio(PRECIO_A_AUMENTAR);
+                        break;
+
+                    case 3:
+                        const PRECIO_A_DISMINUIR = parseFloat(
+                            prompt("Ingrese en cuanto disminuye el precio del producto")
+                        );
+                        miProducto.disminuirPrecio(PRECIO_A_DISMINUIR);
+                        break;
+
+                    default:
+                        alert("Opcion Incorrecta");
+                        break;
+                }
+            } else {
+                alert("Ingresó una letra");
+            }
+        } else {
+            alert("Seleccione la opción");
+        }
+        opcionSeleccionada = mostrarMenu();
+    }
+}
 
 function main() {
     let compras = agregarCompras();
     let costoTotal = calcularCosto(compras);
-    alert(`El costo total de su compra es de: ${costoTotal} UYU.`);
+    alert(`El costo total de su compra es de: ${costoTotal} UYU.\n\nSu carrito contiene:\n\n${JSON.stringify(compras)}`);
     let pagoAdelanto = parseInt(prompt(`Ingrese si desea realizar un pago por adelantado.`))
     let numeroCuotas = parseInt(prompt('Ingrese número de cuotas en que desea pagar (de seleccionar el pago en más de 6 cuotas, aplica un interes del 5%).'));
     let cuota = valorCuota(costoTotal, pagoAdelanto, numeroCuotas)
