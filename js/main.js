1a606498635019935a60bd3af9aaaa2318c7d6c1
Federico Peluffo <fedepel@gmail.com>
2022-10-09 14:40:17 -0300

    Fixed issues

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 //-------------------------------------Variables------------------------------//
 ////////////////////////////////////////////////////////////////////////////////
-// const baseDeDatos = [
-//     { id: 1, nombre: 'Super Mario Odissey', precio: 90, imagen: 'images/super-mario-odyssey.jpg' },
-//     { id: 2, nombre: 'Mario Party Superstars', precio: 90, imagen: 'images/mario-party-superstars.jpg' },
-//     { id: 3, nombre: 'Zelda - Breath of the wild', precio: 90, imagen: 'images/zelda-breath-of-the-wild.jpg' },
-//     { id: 4, nombre: 'Pokemon Legends - ARCEUS', precio: 110, imagen: 'images/pokemon-legends-arceus.jpg' },
-//     { id: 5, nombre: 'Pokemon Scarlet', precio: 120, imagen: 'images/pokemon-scarlet.jpg' },
-//     { id: 6, nombre: 'Pokemon Violet', precio: 120, imagen: 'images/pokemon-violet.jpg' }
-// ];
 
-// Variables para elementos de autenticación y usuario
-
+let baseDeDatos
 let carrito = [];
 const divisa = 'USD';
 const items = document.querySelector('#items');
@@ -297,6 +288,7 @@ function consultarProductosJson() {
     .then((data) => {
         baseDeDatos = [...data]
         renderizarProductos();
+        renderizarCarrito();
     })
     .catch((error) => console.log(error))
 }
@@ -311,7 +303,6 @@ function main() {
     consultarProductosJson();
     obtenerCarritoStorage();
     obtenerUsuarioStorage();
-    renderizarCarrito();
 }
 
 main()
